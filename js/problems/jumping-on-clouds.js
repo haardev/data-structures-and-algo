1bca25aedce68cfccf207b54709d79ed66999c12
Milen Gardev <milenford@gmail.com>
2020-06-06 11:47:05 +0200

    Adds jumping on the clouds

---
+++ js/problems/jumping-on-clouds.js
@@ -0,0 +1,22 @@
+//Jump over clouds
+
+function jumpingOnClouds(listOfnumbers) {
+    let counter = 0;
+    let i = 0;
+
+    while(i < listOfnumbers.length - 1) {
+        if(i + 2 === listOfnumbers.length || listOfnumbers[i + 2] == 1) {
+            counter = counter + 1;
+            i = i + 1;
+        }
+        else {
+            counter = counter + 1;
+            i = i + 2;
+        }
+    }
+}
+
+
+let inputData = [0, 0, 1, 0, 0, 1, 0];
+
+jumpingOnClouds(inputData);
